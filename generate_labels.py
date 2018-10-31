2a9c490df16ed92787b8733fe4185bebd52fb8db
HaZz <magnustboisen@gmail.com>
2018-10-31 14:40:16 +0100

    small fixes

--- generate_labels.py
+++ generate_labels.py
@@ -8,19 +8,20 @@ image_labels = ["cats", "dogs"]
 # Image size convertion of training set
 img_width_height = (150, 150)
 
-training_path = "data/"
+training_path = "data"
 data = []
 labels = []
 
 # Retreiving dataset for classes
 for x in range(0, len(image_labels)):
-  animals = glob(os.path.join(os.getcwd(), training_path, image_labels[x]) + "/*.")
-  for animal in animals:
-      # Reads image in greyscale and resizes
-      image = cv2.imread(animal)
-      image = cv2.resize(image, img_width_height)
-      data.append(image)
-      labels.append(x)
+    dataset_path = os.path.join(os.getcwd(), training_path, image_labels[x])
+    animals = glob(dataset_path + "/*.*")
+    for animal in animals:
+        # Reads image in greyscale and resizes
+        image = cv2.imread(animal)
+        image = cv2.resize(image, img_width_height)
+        data.append(np.array(image))
+        labels.append(x)
 
 # Converts and saves dataset
 animals = np.array(data)
