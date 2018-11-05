ad13e4b348ee679d199d06aa2774e7c31f2483a4
LukeRH <lukasronsholt@gmail.com>
2018-11-05 15:01:02 +0100

    First version of model, ca. 80% acc

--- train_model.py
+++ train_model.py
@@ -1,83 +1,83 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from math import ceil
+from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Conv2D, Activation, Dense, MaxPooling2D, Flatten, Dropout
-from generate_labels import img_width_height as imgsize
+from keras import backend as K
 
-TEST_RATIO = 0.1
-EPOCH_SIZE = 50 # total number of runs
-BATCH_SIZE = 16 # parts to split dataset into
+epoch_size = 10 # total number of runs
+batch_size = 16 # parts to split dataset into
+TRAIN_PATH = 'data/train'
+VALIDATION_PATH = 'data/validation'
 
-def retrieve_dataset():
-    # Loads dataset
-    animals = np.load("data/animals.npy")
-    labels = np.load("data/labels.npy")
+img_width, img_height = 128, 128
 
-    # Shuffles dataset
-    shape = np.arange(animals.shape[0])
-    np.random.shuffle(shape)
-    animals = animals[shape]
-    labels = labels[shape]
-    return animals, labels, shape
-
-def retrieve_training_test_dataset(animals, labels):
-    dataset_size = len(animals)
-    
-    # Splits into test set
-    x_test = animals[0:ceil(dataset_size*TEST_RATIO)]
-    y_test = labels[0:ceil(dataset_size*TEST_RATIO)]
-
-    # Splits into train set
-    x_train = animals[ceil(dataset_size*TEST_RATIO):]
-    y_train = labels[ceil(dataset_size*TEST_RATIO):]
-
-    return x_train, y_train, x_test, y_test
-
-def create_model(shape):    
+def create_model():    
     model = Sequential()
 
-    #https://keras.io/layers/convolutional/#conv2d
-    model.add(Conv2D(imgsize[0], (3, 3), activation='relu', input_shape=(128, 128, 3)))
-    model.add(Conv2D(imgsize[0], (3, 3), activation = 'relu'))
+    model.add(Conv2D(32, (3, 3), input_shape=(img_width, img_height, 3)))
+    model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
 
-    model.add(Conv2D(imgsize[0] * 2, (3, 3), activation='relu'))
-    model.add(Conv2D(imgsize[0] * 2, (3, 3), activation = 'relu'))
+    model.add(Conv2D(32, (3, 3)))
+    model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
 
-    model.add(Conv2D(imgsize[0] * 2, (3, 3), activation='relu'))
-    model.add(Conv2D(imgsize[0] * 2, (3, 3), activation = 'relu'))
+    model.add(Conv2D(64, (3, 3)))
+    model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
 
-    model.add(Flatten())
-    model.add(Dense(imgsize[0] * 2))
+    model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
+    model.add(Dense(64))
     model.add(Activation('relu'))
     model.add(Dropout(0.5))
     model.add(Dense(1))
-    model.add(Activation('softmax'))
+    model.add(Activation('sigmoid'))
 
-    model.compile(loss='binary_crossentropy',
-              optimizer='rmsprop',
-              metrics=['accuracy'])
+    model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
 
-    return model
+    return model    
+
+def retrieve_generators():
+    train_datagen = ImageDataGenerator(rescale=1. / 255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True)
+    test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+    train_generator = train_datagen.flow_from_directory(TRAIN_PATH, target_size=(img_width, img_height), batch_size=batch_size, class_mode='binary')
+    validation_generator = test_datagen.flow_from_directory(VALIDATION_PATH, target_size=(img_width, img_height), batch_size=batch_size, class_mode='binary')
+
+    return train_generator, validation_generator
+
+def plot_model_training(history):
+    # Plot training & validation accuracy values
+    plt.plot(history.history['acc'])
+    plt.plot(history.history['val_acc'])
+    plt.title('Model accuracy')
+    plt.ylabel('Accuracy')
+    plt.xlabel('Epoch')
+    plt.legend(['Train', 'Test'], loc='upper left')
+    plt.show()
+
+    # Plot training & validation loss values
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('Model loss')
+    plt.ylabel('Loss')
+    plt.xlabel('Epoch')
+    plt.legend(['Train', 'Test'], loc='upper left')
+    plt.show()
+
 
 def train_model():
-    animals, labels, shape = retrieve_dataset()
-    model = create_model(shape)
-    
-    x_train, y_train, x_test, y_test = retrieve_training_test_dataset(animals, labels)
+    model = create_model()
+    train_generator, validation_generator = retrieve_generators()
 
-    model.fit(x_train, y_train,
-            epochs=EPOCH_SIZE,
-            batch_size=BATCH_SIZE,
-            validation_data=(x_test, y_test),
-            verbose=1)
+    step_size_train = train_generator.n // train_generator.batch_size
+    step_size_valid = validation_generator.n // validation_generator.batch_size
 
+    history = model.fit_generator(generator=train_generator, steps_per_epoch=step_size_train, validation_data=validation_generator, validation_steps=step_size_valid, epochs=epoch_size)
     model.save("model.h5")
+    plot_model_training(history)
 
 if __name__ == "__main__":
     train_model()
