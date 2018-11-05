ecd89e64312aa9871d62a965f254b54d4d156272
LukeRH <lukasronsholt@gmail.com>
2018-11-05 13:00:31 +0100

    Model changes

--- generate_labels.py
+++ generate_labels.py
@@ -6,26 +6,35 @@ from glob import glob
 # Classes for training set
 image_labels = ["cats", "dogs"]
 # Image size convertion of training set
-img_width_height = (150, 150)
+img_width_height = (128, 128)
 
-training_path = "data"
-data = []
-labels = []
 
-# Retreiving dataset for classes
-for x in range(0, len(image_labels)):
-    dataset_path = os.path.join(os.getcwd(), training_path, image_labels[x])
-    animals = glob(dataset_path + "/*.*")
-    for animal in animals:
-        # Reads image in greyscale and resizes
-        image = cv2.imread(animal)
-        image = cv2.resize(image, img_width_height)
-        data.append(np.array(image))
-        labels.append(x)
+def generate_labels():
+    training_path = "data"
+    data = []
+    labels = []
 
-# Converts and saves dataset
-animals = np.array(data)
-labels = np.array(labels)
+    # Retreiving dataset for classes
+    for x in range(0, len(image_labels)):
+        dataset_path = os.path.join(os.getcwd(), training_path, image_labels[x])
+        animals = glob(dataset_path + "/*.*")
+        for animal in animals:
+            # Reads image in greyscale and resizes
+            image = cv2.imread(animal)
+            image = cv2.resize(image, img_width_height)
+            data.append(np.array(image))
+            labels.append(x)
+            if (len(data) % 500 == 0):
+                print(len(data))
 
-np.save(os.path.join(training_path, "animals"), animals)
-np.save(os.path.join(training_path, "labels"), labels)
+    # Converts and saves dataset
+    animals = np.array(data)
+    labels = np.array(labels)
+
+    print("Saving results, this might take some time")
+    np.save(os.path.join(training_path, "animals"), animals)
+    np.save(os.path.join(training_path, "labels"), labels)
+
+
+if __name__ == "__main__":
+    generate_labels()
--- train_model.py
+++ train_model.py
@@ -1,11 +1,12 @@
 import numpy as np
 from math import ceil
 from keras.models import Sequential
-from keras.layers import Conv2D, Activation, Dense
-
+from keras.layers import Conv2D, Activation, Dense, MaxPooling2D, Flatten, Dropout
 from generate_labels import img_width_height as imgsize
 
 TEST_RATIO = 0.1
+EPOCH_SIZE = 50 # total number of runs
+BATCH_SIZE = 16 # parts to split dataset into
 
 def retrieve_dataset():
     # Loads dataset
@@ -36,17 +37,47 @@ def create_model(shape):
     model = Sequential()
 
     #https://keras.io/layers/convolutional/#conv2d
-    model.add(Conv2D(imgsize[0], (3,3), padding='same', activation='relu', input_shape=shape))
+    model.add(Conv2D(imgsize[0], (3, 3), activation='relu', input_shape=(128, 128, 3)))
+    model.add(Conv2D(imgsize[0], (3, 3), activation = 'relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Conv2D(imgsize[0] * 2, (3, 3), activation='relu'))
+    model.add(Conv2D(imgsize[0] * 2, (3, 3), activation = 'relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Conv2D(imgsize[0] * 2, (3, 3), activation='relu'))
+    model.add(Conv2D(imgsize[0] * 2, (3, 3), activation = 'relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Flatten())
+    model.add(Dense(imgsize[0] * 2))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(1))
+    model.add(Activation('softmax'))
+
+    model.compile(loss='binary_crossentropy',
+              optimizer='rmsprop',
+              metrics=['accuracy'])
 
     return model
-
 
 def train_model():
     animals, labels, shape = retrieve_dataset()
     model = create_model(shape)
+    
     x_train, y_train, x_test, y_test = retrieve_training_test_dataset(animals, labels)
 
+    model.fit(x_train, y_train,
+            epochs=EPOCH_SIZE,
+            batch_size=BATCH_SIZE,
+            validation_data=(x_test, y_test),
+            verbose=1)
 
+    model.save("model.h5")
 
 if __name__ == "__main__":
     train_model()
