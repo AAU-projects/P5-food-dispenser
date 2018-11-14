4f0784493e9c4d91b67a86318b60ece38c7d6db1
HaZz <magnustboisen@gmail.com>
2018-11-14 14:45:18 +0100

    automaticaly generate

--- run.py
+++ run.py
@@ -1,5 +1,11 @@
+import os
 from src.train_model import TrainModel
+from src.image_processing import ImageProcessing
 
 if __name__ == "__main__":
+    if not os.path.exists(path_dataset):
+        print("[LOG] No npy files found... Generating")
+        ip = ImageProcessing()
+        ip.generate_labels()
     tm = TrainModel()
     tm.train_model()
