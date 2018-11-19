70e89b101c95d01b4170a847f355162cb8d5c072
Raspberry Pi <lukasronsholt@gmail.com>
2018-11-19 12:56:09 +0000

    Updated the temp.py to python 3

--- temp.py
+++ temp.py
@@ -1,9 +1,9 @@
 import os
 import time
 
-temp = 0
-high = 0
-low = "h"
+temp = 0.0
+high = 0.0
+low = 100.0
 
 
 def clear():
@@ -15,16 +15,17 @@ def measure_temp():
 
 	temp = os.popen("vcgencmd measure_temp").readline()
 	temp = temp.replace("temp=","")
-	if temp < low:
+	temp = temp.replace("'C\n", "")
+	if float(temp) < float(low):
 		low = temp
 
-	if temp > high:
+	if float(temp) > float(high):
 		high = temp
 while True:
 	
 	measure_temp()
-	if temp >= 85:
+	if float(temp) >= 85.0:
 		os.system("sudo shutdown now -h")
-	print "Current temp: " +str(temp) + "\nHighest: " +str(high) + "\nLowest: " +str(low)
-	time.sleep(2)
+	print("Current temp: " +str(temp) + "\nHighest: " +str(high) + "\nLowest: " +str(low))
+	time.sleep(1)
 	clear()
