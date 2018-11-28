bc24b40fdc87c7f44cecc0090b1a21799af41ec5
HaZz <magnustboisen@gmail.com>
2018-11-28 15:14:05 +0100

    rl now working :)

--- predict_dispenser.py
+++ predict_dispenser.py
@@ -1,32 +1,20 @@
 import keras
-import test
 import numpy as np
+from test import FoodDispenser, DispenseAgent
 
-#Setup
-env = test.FoodDispenser()
-agent = test.DispenseAgent(3, 2)
-agent.load("food_dispenser.h5")
-
-env = test.FoodDispenser()
+# Setup.
+env = FoodDispenser()
+env.predict = 1
 state_size = env.state_size
 action_size = env.action_size
-agent = test.DispenseAgent(state_size, action_size)
+
+agent = DispenseAgent(state_size, action_size) 
+agent.load("food_dispenser.h5")
 
 
-# Predict
-
-# 0 for cat
-# 1 for dog
-state = env.predict_functions(0)
-
-state = np.reshape(state, [1, state_size])
-for step in range(2):
-    action = agent.act(state)
-    next_state, reward, done = env.step(action)
-
-    reward = reward if done else -1
-    next_state = np.reshape(next_state, [1, state_size])
-    agent.remember(state, action, reward, next_state, done)
-    state = next_state
-    print(state)
-            
+for x in range(0, 2):
+    state = [env.predict, 0, 0]
+    state = np.reshape(state, [1, state_size])
+    result = agent.predict(state)
+    next_state, reward, done = env.step(result)
+    print(next_state)
