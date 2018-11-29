a5fa2ef83e4e021182b2383aa46a029f9cb2a6e9
LukeRH <lukasronsholt@gmail.com>
2018-11-29 09:58:57 +0100

    Refactoring files, and addition of th. class

--- src/image_processing.py
+++ src/image_processing.py
@@ -7,34 +7,38 @@ from glob import glob
 from PIL import Image
 from keras.utils import np_utils, to_categorical
 
-
 class ImageProcessing:
+
     def __init__(self, *args, **kwargs):
-        self.image_labels = ["cats", "dogs"]
-        self.training_data_path = "data"
+        self.image_labels = ["cats", "dogs", "junk"]
+        self.training_data_path = "new data"
         self.img_width_height = (128, 128)
-        self.generate_folders = ['dataset', 'test']
+        self.picture_folders = ['dataset', 'evalset']
+        self.num_classes = 3
 
     def __load_image(self, filepath):
         img = cv2.imread(filepath)
-        img_from_ar = Image.fromarray(img, 'RGB')
-        resized_image = img_from_ar.resize(self.img_width_height)
+        try:
+            img_from_ar = Image.fromarray(img, 'RGB')
+            resized_image = img_from_ar.resize(self.img_width_height)
 
-        return np.array(resized_image)
+            return np.array(resized_image)
+        except AttributeError:
+            print(f"Bad file: {filepath}")
+            return None
 
-    def __load_images(self, dataset_type, folder):
-        print("[LOG] Loading " + dataset_type)
-        folder_path = os.path.join(
-            folder, self.training_data_path, dataset_type)
+    def __load_images(self, picture_folder, base_path):
+        print("[LOG] Loading " + picture_folder)
+        folder_path = os.path.join(base_path, self.training_data_path, picture_folder)
         data, labels = self.__load_type(folder_path)
 
         if len(data) > 0:
-            animals = np.array(data)
+            pictures = np.array(data)
             labels = np.array(labels)
 
             print("[LOG] Saving results, this might take some time")
-            np.save(os.path.join(folder_path, "animals_" + dataset_type), animals)
-            np.save(os.path.join(folder_path, "labels_" + dataset_type), labels)
+            np.save(os.path.join(folder_path, "pictures_" + picture_folder), pictures)
+            np.save(os.path.join(folder_path, "labels_" + picture_folder), labels)
 
     def __load_type(self, training_path):
         data = []
@@ -47,62 +51,62 @@ class ImageProcessing:
             if not os.path.exists(dataset_path):
                 print(f"[ERROR] Requires data folder at {dataset_path}")
                 continue
-            animals = glob(dataset_path + "/*.*")
-            for animal in animals:
-                data.append(self.__load_image(animal))
-                labels.append(x)
+            pictures = glob(dataset_path + "/*.*")
+            for picture in pictures:
+                img = self.__load_image(picture)
+                if img is not None:
+                    data.append(img)
+                    labels.append(x)
                 if (len(data) % 500 == 0):
                     print(f"[LOG] {len(data)}")
 
         return data, labels
 
     def generate_labels(self, path=os.getcwd()):
-        for i in range(0, len(self.generate_folders)):
-            self.__load_images(self.generate_folders[i], path)
+        for i in range(0, len(self.picture_folders)):
+            self.__load_images(self.picture_folders[i], path)
 
+    def retrive_dataset_old(self, folder):
+        print("Loading images")
+        pictures = np.load(os.path.join(self.training_data_path, folder, f"pictures_{folder}.npy"))
+        labels = np.load(os.path.join(self.training_data_path, folder, f"labels_{folder}.npy"))
 
-    def retrive_dataset_old(self, datatype):
-        print("Loading animals")
-        animals = np.load(f"data/{datatype}/animals_{datatype}.npy")
-        labels = np.load(f"data/{datatype}/labels_{datatype}.npy")
-
-        return animals, labels
+        return pictures, labels
         
     def retrive_dataset_test(self):
-        print("Loading animals")
-        animals, labels = self.retrive_dataset_old("test")
-        animals, labels = self.__shuffle(animals, labels)
-        animals = animals.astype('float32')/255
+        print("Loading images")
+        pictures, labels = self.retrive_dataset_old(self.picture_folders[1])
+        pictures, labels = self.__shuffle(pictures, labels)
+        pictures = pictures.astype('float32')/255
 
-        num_classes = 2
         # One hot encoding
         print("One hot encoding")
-        labels = keras.utils.to_categorical(labels, num_classes)
+        labels = keras.utils.to_categorical(labels, self.num_classes)
 
-        return animals, labels
+        return pictures, labels
         
-
     def retrieve_train_validation(self, shuffle=True, procent_split=0.9):
-        animals, labels = self.retrive_dataset_old("dataset")
+        pictures, labels = self.retrive_dataset_old(self.picture_folders[0])
+        pictures, labels = self.__shuffle(pictures, labels)
+        train_size = int(len(pictures) * procent_split)
 
-        animals, labels = self.__shuffle(animals, labels)
-
-        train_size = int(len(animals) * procent_split)
-        animals_train = animals[:train_size]
+        # Get training set
+        pictures_train = pictures[:train_size]
         labels_train = labels[:train_size]
-        animals_validation = animals[train_size:]
+        # Get validation set
+        pictures_validation = pictures[train_size:]
         labels_validation = labels[train_size:]
 
-        animals_train = animals_train.astype('float32')/255
-        animals_validation = animals_validation.astype('float32')/255
+        # Format picrures to right format
+        pictures_train = pictures_train.astype('float32')/255
+        pictures_validation = pictures_validation.astype('float32')/255
 
-        num_classes = 2
         # One hot encoding
         print("One hot encoding")
-        labels_train = keras.utils.to_categorical(labels_train, num_classes)
-        labels_validation = keras.utils.to_categorical(labels_validation, num_classes)
+        labels_train = keras.utils.to_categorical(labels_train, self.num_classes)
+        labels_validation = keras.utils.to_categorical(labels_validation, self.num_classes)
 
-        return animals_train, labels_train, animals_validation, labels_validation
+        return pictures_train, labels_train, pictures_validation, labels_validation
 
     def __shuffle(self, x, y):
         shape = np.arange(x.shape[0])
@@ -110,7 +114,6 @@ class ImageProcessing:
         x = x[shape]
         y = y[shape]
         return x, y
-
 
 if __name__ == "__main__":
     gen = ImageProcessing()
--- src/train_model.py
+++ src/train_model.py
@@ -15,18 +15,16 @@ class TrainModel:
         self.ip = ImageProcessing()
         self.eval = ModelEvaluate()
 
-        self.epoch_size = 2 # total number of runs
+        self.epoch_size = 10   # total number of runs
         self.batch_size = 128 # parts to split dataset into
         self.dataset_split_percentage = 0.9
-        self.number_of_classes = 2
-        self.TRAIN_PATH = 'data/train'
-        self.VALIDATION_PATH = 'data/validation'
+        self.number_of_classes = 3
         self.training_lenght = 0
         self.validation_lenght = 0
         self.img_width, self.img_height = 128, 128
 
     def __create_model(self):
-        '''
+        
         model = Sequential()
 
         model.add(Conv2D(32, (3, 3), input_shape=(self.img_width, self.img_height, 3)))
@@ -56,76 +54,58 @@ class TrainModel:
         model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
         model.add(Dense(512))
         model.add(Activation('relu'))
-        model.add(Dense(2))
+        model.add(Dense(self.number_of_classes))
         model.add(Activation('softmax'))
 
         model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy', 'categorical_crossentropy'])
 
-        return model  
-        '''
-
-        model = Sequential()
-        model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(128, 128, 3)))
-        model.add(MaxPooling2D((2, 2)))
-        model.add(Conv2D(64, (3, 3), activation='relu'))
-        model.add(MaxPooling2D((2, 2)))
-        model.add(Conv2D(64, (3, 3), activation='relu'))
-        model.add(Flatten(name="intermediate_output"))
-        model.add(Dense(64, activation='relu'))
-        model.add(Dense(2, activation='softmax'))
-        
-        # The extra metric is important for the evaluate function
-        model.compile(optimizer='rmsprop',
-                    loss='categorical_crossentropy',
-                    metrics=['accuracy'])
         return model
 
-    def __fit_model_numpy(self, model, callbacks=None):
-        animals_train, labels_train, animals_validation, labels_validation = self.__retrieve_dataset()
-        history = model.fit(animals_train, labels_train, batch_size=self.batch_size, epochs=self.epoch_size, verbose=1, validation_data=(animals_validation, labels_validation), callbacks=callbacks)
+    def __fit_model(self, model, callbacks=None):
+        # Get images
+        pictures_train, labels_train, pictures_validation, labels_validation = self.__retrieve_dataset()
 
+        # Start the training
+        history = model.fit(pictures_train, labels_train, batch_size=self.batch_size, epochs=self.epoch_size, verbose=1, validation_data=(pictures_validation, labels_validation), callbacks=callbacks)
+
+        # Evaluate the model
         score = self.eval.evaluate_model(model)
         return history, score
 
     def __retrieve_dataset(self):
-        animals_train, labels_train, animals_validation, labels_validation = self.ip.retrieve_train_validation(procent_split=self.dataset_split_percentage)
+        pictures_train, labels_train, pictures_validation, labels_validation = self.ip.retrieve_train_validation(procent_split=self.dataset_split_percentage)
 
-        self.training_lenght = len(animals_train)
-        self.validation_lenght = len(animals_validation)
+        # Number of training pictures
+        self.training_lenght = len(pictures_train)
+        # Number of validation pictures
+        self.validation_lenght = len(pictures_validation)
 
-        return animals_train, labels_train, animals_validation, labels_validation
-
-    def SVMWrapper(self, model):
-        return ModelSVMWrapper(model)
-
-    def TrainSVM(self, wrapper, callbacks=None):
-        animals_train, labels_train, animals_validation, labels_validation = self.__retrieve_dataset()
-        history = wrapper.fit(animals_train, labels_train, batch_size=self.batch_size, epochs=self.epoch_size, verbose=1, validation_data=(animals_validation, labels_validation), callbacks=callbacks)
-
-        score = self.eval.evaluate_model(wrapper.model)
-        return history, score
+        return pictures_train, labels_train, pictures_validation, labels_validation
 
     def train_model(self):
         model = self.__create_model()
 
-        #TensorBoard
+        # Tensor Callbacks, TensorBoard and Early stopping
         tensorboard_name = str(time())
         tensorboard = TensorBoard(log_dir="logs/{}".format(tensorboard_name));
         earlyStop = EarlyStopping(monitor='val_loss', min_delta=0, patience=20, verbose=1, mode='auto', restore_best_weights=True)
         
-        history, score = self.__fit_model_numpy(model, [tensorboard, earlyStop])
-        # history, score = self.TrainSVM(self.SVMWrapper(model), [tensorboard, earlyStop]) 
+        # Train the model
+        history, score = self.__fit_model(model, [tensorboard, earlyStop])
 
-        #plot_history([('model', history)])
+        # Generate the name of the just trained model, and get the file location to store it at
         score_rounded = f"{round(score[1], 3):.3f}"
         model_name = FileSystem.get_model_name(score_rounded, self.epoch_size, self.batch_size)
         model_path = FileSystem.get_model_path(model_name)
 
+        # Save the model to disk
         print("[LOG] New model saved in " + model_path)
         model.save(model_path + model_name + ".h5")
+
+        # Create graphs of model performance
         Graphs().plot_model(history, model_path, model_name)
 
-        # Saves model
+        # Save model summery
         lines = inspect.getsource(self.__create_model)
         FileSystem.save_model_summary(lines, model_path, model_name, self.training_lenght, 
                                         self.validation_lenght)
@@ -133,7 +113,6 @@ class TrainModel:
         # Renames model log
         FileSystem.rename_model_log(tensorboard_name, model_name)
         
-
 
 if __name__ == "__main__":
     tm = TrainModel()
